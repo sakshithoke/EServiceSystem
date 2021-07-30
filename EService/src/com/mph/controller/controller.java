97135f793f6fb60806086c603ecb99e53f079808
sak <sakshithoke@gmail.com>
2021-07-30 17:08:10 +0530

    adding controller

---
+++ EService/src/com/mph/controller/controller.java
@@ -0,0 +1,8 @@
+package com.mph.controller;
+
+public class Controller
+{
+	
+}
+
+	
--- EService/src/com/mph/view/ViewClass.java
+++ EService/src/com/mph/view/ViewClass.java
@@ -1,4 +1,4 @@
-package com.view;
+package com.mph.view;
 public class ViewClass
 {
 	public static void main(String[] args)
